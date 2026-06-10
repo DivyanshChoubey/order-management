3e86fa5afb925e12f56de72181101a3f23cf69b7
DivyanshChoubey <divyansh.choubey01@gmail.com>
2026-06-10 09:54:36 +0530

    chore: update settings and env

--- order_management/settings.py
+++ order_management/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6)#l*!f-m9s0r+i&86r)=xn7*ibcn7xwq0ig%@@#$x_c#1u+rb'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'service',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
